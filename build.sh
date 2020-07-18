2fa39ba94714267460e8e9c95990185c31709ca4
Jeremy Sawicki <jeremy-icfp2020@sawicki.us>
2020-07-18 01:36:29 -0700

    Remove cpu/mem info

--- build.sh
+++ build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-echo ======== cat /proc/meminfo ========
-cat /proc/meminfo
-echo ======== lscpu ========
-lscpu
-echo ======== cat /proc/cpuinfo ========
-cat /proc/cpuinfo
-cd src
 DOCKER=1 make app
 DOCKER=1 make almostclean
--- run.sh
+++ run.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-echo ======== cat /proc/meminfo ========
-cat /proc/meminfo
-echo ======== lscpu ========
-lscpu
-echo ======== cat /proc/cpuinfo ========
-cat /proc/cpuinfo
-cd src
 exec ./app -v "$@"
